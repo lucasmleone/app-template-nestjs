e441e37281be3dad8358d6a887339b518836313b
Lucas Leone <lucasmleone@gmail.com>
2025-07-14 22:40:08 -0300

    get hello to check http endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.connection.readyState === 1 ? "MongoDB Connected!" : "MongoDB Not Connected!";
   }
 }
